124309ffca932ff4974333a0cffc9fece5fe2fa9
Haleem Hosein <haleemhosein@yahoo.com>
2022-07-07 20:24:15 -0400

    09 Actions as commands

--- app/Actions/SendOrderEmail.php
+++ app/Actions/SendOrderEmail.php
@@ -3,13 +3,16 @@
 namespace App\Actions;
 
 use App\Models\Order;
-use Lorisleiva\Actions\Action;
 use App\Mail\OrderCreatedMail;
+use Illuminate\Console\Command;
 use App\Events\OrderCreatedEvent;
 use Illuminate\Support\Facades\Mail;
+use Lorisleiva\Actions\Concerns\AsAction;
 
-class SendOrderEmail extends Action
+class SendOrderEmail
 {
+    use AsAction;
+
     public function handle(Order $order)
     {
         Mail::to($order->user)->send(new OrderCreatedMail($order));
@@ -18,5 +21,17 @@ class SendOrderEmail extends Action
     public function asListener(OrderCreatedEvent $event): void
     {
         $this->handle($event->order);
+    }
+
+    public function asCommand(Command $command)
+    {
+        $this->handle(Order::findOrFail($command->argument('order_id')));
+
+        $command->info('Email sent');
+    }
+
+    public function getCommandSignature(): string
+    {
+        return 'orders:send-order-email {order_id}';
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,16 @@
 
 namespace App\Console;
 
+use App\Actions\SendOrderEmail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        SendOrderEmail::class
+    ];
+
     /**
      * Define the application's command schedule.
      *
